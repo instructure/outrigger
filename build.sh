c3cd3298742fc9159fe1def71446bc313c61e6f8
Brian Palmer <brianp@instructure.com>
2016-05-25 15:25:15 -0600

    dockerize ci build
    Change-Id: I2c6b3ea31968a507e132abf8ec4422d98d9c17cd

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
-. ~/.bash_profile
-export COVERAGE=1
-rvm use ruby-2.1.5 2>/dev/null || :
 
-gem install bundler
-bundle install
-bundle exec rspec
+set -e
+
+docker run --rm -v $(pwd):/usr/src/app instructure/ruby:2.1 /bin/bash -c "bundle install && bundle exec rspec"
